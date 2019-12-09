851f9b538ba84a9c66b58a56cfe763cada6fa066
fbepdor <fbepdor@yandex.ru>
2019-12-09 21:05:39 +0300

    25 level 01 - 03

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2503/Column.java
@@ -0,0 +1,99 @@
+package com.javarush.task.task25.task2503;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public enum Column implements Columnable{
+    Customer("Customer"),
+    BankName("Bank Name"),
+    AccountNumber("Account Number"),
+    Amount("Available Amount");
+
+    private String columnName;
+
+    private static int[] realOrder;
+
+    private Column(String columnName) {
+        this.columnName = columnName;
+    }
+
+    /**
+     * Задает новый порядок отображения колонок, который хранится в массиве realOrder.
+     * realOrder[индекс в энуме] = порядок отображения; -1, если колонка не отображается.
+     *
+     * @param newOrder новая последовательность колонок, в которой они будут отображаться в таблице
+     * @throws IllegalArgumentException при дубликате колонки
+     */
+    public static void configureColumns(Column... newOrder) {
+        realOrder = new int[values().length];
+        for (Column column : values()) {
+            realOrder[column.ordinal()] = -1;
+            boolean isFound = false;
+
+            for (int i = 0; i < newOrder.length; i++) {
+                if (column == newOrder[i]) {
+                    if (isFound) {
+                        throw new IllegalArgumentException("Column '" + column.columnName + "' is already configured.");
+                    }
+                    realOrder[column.ordinal()] = i;
+                    isFound = true;
+                }
+            }
+        }
+    }
+
+    /**
+     * Вычисляет и возвращает список отображаемых колонок в сконфигурированом порядке (см. метод configureColumns)
+     * Используется поле realOrder.
+     *
+     * @return список колонок
+     */
+    public static List<Column> getVisibleColumns() {
+        List<Column> result = new LinkedList<>();
+        Column[] list = Column.values();
+        for (int i = 0; i < list.length; i++) {
+            for (int j = 0; j < list.length; j++){
+                if (realOrder[j] == i) {
+                    result.add(list[j]);
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+//5. Метод Column.getVisibleColumns должен возвращать список отображаемых колонок в скофигурированом порядке.
+//6. Метод Column.getColumnName должен возвращать полное имя колонки.
+//7. Метод Column.isShown должен возвращать true, если колонка видимая, иначе false.
+//8. Метод Column.hide должен скрывать колонку и сдвигать индексы остальных отображаемых колонок.
+    @Override
+    public String getColumnName() {
+        /**
+         * @return полное имя колонки
+         */
+        return this.columnName;
+    }
+
+    @Override
+    public boolean isShown() {
+        /**
+         * Возвращает true, если колонка видимая, иначе false
+         */
+        return realOrder[this.ordinal()] != -1 ;
+    }
+
+    @Override
+    public void hide() {
+/**
+ * Скрывает колонку - маркирует колонку -1 в массиве realOrder.
+ * Сдвигает индексы отображаемых колонок, которые идут после скрытой
+ */
+        int startNumberToChange = realOrder[this.ordinal()];
+        realOrder[this.ordinal()] = -1;
+        for (int i = 0; i < realOrder.length; i++) {
+            if ( realOrder[i] > startNumberToChange) {
+                realOrder[i]--;
+            }
+        }
+    }
+}
